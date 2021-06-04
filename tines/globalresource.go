dac30ec26cff065662df5c9d23ce3c9753fd7e76
tuckner <tuckner329@gmail.com>
2021-06-03 22:02:35 -0500

    remove valuetype

--- tines/globalresource.go
+++ tines/globalresource.go
@@ -24,7 +24,6 @@ type GlobalResource struct {
 	ID         int       `json:"id" structs:"id,omitempty"`
 	UserID     int       `json:"user_id" structs:"user_id,omitempty"`
 	Name       string    `json:"name" structs:"name,omitempty"`
-	ValueType  string    `json:"value_type" structs:"value_type,omitempty"`
 	Value      string    `json:"value" structs:"value,omitempty"`
 	ReadAccess string    `json:"read_access" structs:"read_access,omitempty"`
 	CreatedAt  time.Time `json:"created_at" structs:"created_at,omitempty"`
